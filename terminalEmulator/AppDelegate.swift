976644bbfee664060271dba7a42605e53a49c13e
BuildTools <m.bradford.gago@me.com>
2017-02-21 10:34:47 +0000

    Initial Commit

---
+++ terminalEmulator/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  terminalEmulator
+//
+//  Created by Martin Bradford Gago on 21/02/2017.
+//  Copyright © 2017 Martin Bradford Gago. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
